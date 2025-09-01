18c874fa9c7d425f039dcea29c3f4dd7a9defb03
crdtrd <crdtrd@gmail.com>
2025-08-31 18:21:34 -0700

    Initial commit

---
+++ src/main/java/com/drtdrc/spawneggdrops/SpawnEggDrops.java
@@ -0,0 +1,10 @@
+package com.drtdrc.spawneggdrops;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SpawnEggDrops implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
